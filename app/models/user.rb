77894dc72c6f039df20ac3803e84aa6442ec021c
benoit mahÃe <benoitmahe5@gmail.com>
2023-02-10 23:38:36 +0100

    update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :email, :phone_number, :password, :adress, presence: true
+  validates :email, uniqueness: true, format: { with: /\A.*@.*\.com\z/ }
+  validates :phone_number, uniqueness: true, format: { with: /^0[1-9]([-. ]?[0-9]{2}){4}$/ }
+  validates :last_name, :first_name, length: { minimum: 2 }
 end
