1ba9fbfee219ac5407b50b04a4807d1ca6ec71c6
benoit mahÃe <benoitmahe5@gmail.com>
2023-02-11 00:26:16 +0100

    update products_controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,31 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  skip_before_action :authenticate_owner!
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :items, only: %i[index new create]
+  resources :products, only: %i[index new create show]
 end
